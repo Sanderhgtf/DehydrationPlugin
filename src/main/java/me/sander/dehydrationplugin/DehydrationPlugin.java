473f227f169ef718eff81e76e1f3d7bd4de908d8
BuildTools <Sanderhgtf@gmail.com>
2023-11-14 01:08:01 +0100

    It now sends up a raycast to check for shade at day time.

--- src/main/java/me/sander/dehydrationplugin/DehydrationPlugin.java
+++ src/main/java/me/sander/dehydrationplugin/DehydrationPlugin.java
@@ -1,30 +1,49 @@
 package me.sander.dehydrationplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-/////
 
 public class DehydrationPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Register the task to run every second (20 ticks)
+
+        // Determines the loop time.
         Bukkit.getScheduler().runTaskTimer(this, this::checkSunExposure, 0, 20);
     }
 
     private void checkSunExposure() {
-        // Iterate through all online players
         Bukkit.getOnlinePlayers().forEach(player -> {
             World world = player.getWorld();
-
-            // Check if it's daytime in the player's world
-            if (world.getTime() >= 0 && world.getTime() < 12300) { // You may adjust these values based on your needs
-                // Send message to the console
-                Bukkit.getServer().getConsoleSender().sendMessage("Player " + player.getName() + " is exposed to the sun!");
+            if (world.getTime() >= 0 && world.getTime() < 12300) {
+                int blocksInShade = getBlocksInShade(player.getLocation());
+                Bukkit.getServer().getConsoleSender().sendMessage(player.getName() + " has " + blocksInShade + " blocks of shade.");
             }
         });
+    }
+
+    private int getBlocksInShade(Location location) {
+        World world = location.getWorld();
+        int x = location.getBlockX();
+        int y = location.getBlockY() + 1;
+
+        // Initial blocks of shade. This resembles 100% shade. If you change this you also need to change the raycast grid size.
+        int blocksInShade = 25;
+        // Determines how high up the raycasts go till they stop checking. Lower equals better ram usage.
+        int checkHeight = 30;
+
+        for (int offsetX = -2; offsetX <= 2; offsetX++)
+            for (int offsetZ = -2; offsetZ <= 2; offsetZ++)
+                for (int offsetY = 0; offsetY < checkHeight; offsetY++) {
+                    Block block = world.getBlockAt(x + offsetX, y + offsetY, location.getBlockZ() + offsetZ);
+                    if (!block.getType().isTransparent()) break;
+                    if (offsetY == (checkHeight-1)) blocksInShade--;
+                }
+
+        return blocksInShade;
     }
 }
